422750d7af0e912505937e1661c0b0d5f4b81dae
Kris Kristofferson Nabua <kc.nabua@goabroad.com>
2018-11-26 11:35:07 +0800

    fix context bookings userprofile

--- src/context/Bookings.js
+++ src/context/Bookings.js
@@ -2,6 +2,7 @@ import React from 'react'
 import RootComponentContext from './RootComponentContext'
 import { database } from '../firebase/firebase'
 import Loading from '../components/reusables'
+import { assign } from 'lodash'
 
 const BookingsContext = React.createContext()
 
@@ -11,24 +12,36 @@ const BookingsProvider = class extends React.Component {
 	}
 
 	fetchUserProfile = uid => {
-		database
+		this.controller = database
 			.ref()
 			.child('userprofile')
 			.child(uid)
 			.on('value', snapshot => {
-				this.setState({
-					userprofile: snapshot.val()
-				})
+				let userprofile = snapshot.val()
+				assign(userprofile, { uid })
+				this.setState({ userprofile })
 			})
+	}
+
+	componentWillUnmount() {
+		this.controller = null
 	}
 
 	render() {
 		return (
 			<RootComponentContext.Consumer>
 				{({ loggedInUser }) => {
-					if (this.state.userprofile === null) {
+					if (!!!this.state.userprofile) {
 						if (!!loggedInUser)
 							this.fetchUserProfile(loggedInUser.uid)
+					} else if (!!this.state.userprofile) {
+						if (!!loggedInUser) {
+							if (
+								this.state.userprofile.uid != loggedInUser.uid
+							) {
+								this.fetchUserProfile(loggedInUser.uid)
+							}
+						}
 					}
 					return (
 						<BookingsContext.Provider
